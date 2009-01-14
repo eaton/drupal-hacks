b10e4c50f7dc7bc8aa1bdaa2d39e14ed7bf2c779
Jeff Eaton <jeff@viapositiva.net>
2009-01-13 23:32:00 -0600

    Adding a small bundle of novelty modules, and the strangling horror that is LOLTemplate.

---
+++ wrongness/loltemplate/page.lol.php
@@ -0,0 +1,44 @@
+<html><head>
+<title>HAI!1! VISIBLE $head_title!1! KTHXBYE</title>
+HAI!!!
+VISIBLE $head
+VISIBLE $styles
+VISIBLE $scripts
+KTHXBYE
+</head><body>
+
+<div id="page">
+<div id="header">
+HAI
+BTW conditionally print the site name
+$site_name ORL?
+YARLY
+VISIBLE "<h1 id='site-name'>"
+VISIBLE "<a href='" SMOOSH $front_page SMOOSH "' title='Home' rel='home'><span>"
+VISIBLE $site_name SMOOSH "</span></a></h1>"
+OIC!1!
+
+IZ $header
+VISIBLE $header
+KTHX
+KTHXBYE
+</div>
+<div id="sidebar-left" class="column sidebar">HAI!!! VISIBLE $left??! KTHXBYE</div>
+<div id="breadcrumb">HAI!!! VISIBLE $breadcrumb??! KTHXBYE</div>
+
+<div id="content">
+<h1 class="title" id="page-title">HAI!!! VISIBLE $title??! KTHXBYE</h1>
+
+HAI!!!
+VISIBLE $tabs!1!
+$messages O RLY?
+YA RLY, VISIBLE $messages
+OIC
+
+BTW main content goes here
+VISIBLE $content
+KTHXBYE
+
+</div></div>
+</body></html>
+
---
+++ wrongness/loltemplate/sample.lol
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>HAI!!1 VISIBLE $title, KTHXBYE!!!</title>
+</head>
+<body>
+  HAI
+
+"L" WTF?
+  OMG "R"
+    VISIBLE "RED FISH"
+    GTFO
+  OMG "Y"
+    VISIBLE "YELLOW FISH"
+  OMG "G"
+  OMG "B"
+    VISIBLE "FISH HAS A FLAVOR"
+    GTFO
+  OMGWTF
+    VISIBLE "FISH IS TRANSPARENT"
+OIC
+KTHXBYE   
+</body>
+</html>
